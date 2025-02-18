904bc1e26639243e72afc575599bbcf595b936be
Abderrahmanei Moujar <abderrahmanemoujar@gmail.com>
2025-02-18 10:13:35 +0100

    feat:init struct base neural class and abstract Layer class

--- NeuralLeet/nn/module.py
+++ NeuralLeet/nn/module.py
@@ -1,4 +1,21 @@
-'''
-This is the base class that all neural network modules should inherit from.
+from abc import ABC, abstractmethod
+import numpy as np
 
-'''
+class Neural:
+    """Base class for all neural network modules."""
+    def __init__(self) -> None:
+        pass
+
+class Layer(Neural, ABC):
+    """Abstract base class for all neural network layers."""
+    
+    def __init__(self) -> None:
+        super().__init__()
+        
+    @abstractmethod
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        pass
+    
+    @abstractmethod
+    def backward(self, grad_output: np.ndarray) -> np.ndarray:
+        pass
