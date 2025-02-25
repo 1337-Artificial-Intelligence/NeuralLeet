a6edfea0653409cf1b4db0171bdd1772c17a9e5d
khalid benlyazid <seven@khalids-MacBook-Air-1318.local>
2025-02-25 11:40:42 +0000

    feat: start working on the train function of the base model of nn

--- NeuralLeet/nn/mlp.py
+++ NeuralLeet/nn/mlp.py
@@ -1,18 +1,57 @@
-from NeuralLeet.nn.module import NeuralNetwork
+from NeuralLeet.nn.module import Layer, NeuralNetwork
 import numpy as np
 
 
-class mlp(NeuralNetwork):
-    def __init__(
-            self, number_of_layers: int, input_size: int,
-            output_size: int, learning_rate: float = 0.01,
-            batch_size: int = 128):
+class MlpLayer(Layer):
+    '''
+    Multi-layer perceptron layer class, inherits from the Layer class.
+    Parameters:
+        - num_neurons: int
+            The number of neurons in the layer.
+        - num_inputs: int
+            The number of inputs to the each neuron in the current layer.
 
-        # * Initialize the parent class NeuralNetwork
-        super(mlp, self).__init__(number_of_layers, input_size, output_size,
-                                  learning_rate, batch_size)
+    '''
+
+    def __init__(self, num_neurons, num_inputs):
+        super().__init__(num_neurons, num_inputs)
 
     # * Override the forward method
+    def forward(self, x_batch):
+        '''
+        Forward pass for the layer.
+        Parameters:
+            - x_batch: np.ndarray
+                The input batch of data.
+        Returns:
+            - np.ndarray
+                The output of the layer
+        '''
+        results = np.zeros((x_batch.shape[0], self.num_neurons))
+        for i in range(x_batch.shape[0]):
+            predictions_i = [
+                np.dot(neuron.weights, x_batch[i]) + neuron.bias
+                for neuron in self.neurons]
+            results[i] = predictions_i
+        return results
+
+    def backward(self, grad_output):
+        pass
+
+
+class Mlp(NeuralNetwork):
+    def __init__(
+            self, num_hidden_layers: int,
+            num_neurons_hidden: int, input_size: int,
+            output_size: int, learning_rate: float = 0.01,
+            batch_size: int = 128, epochs: int = 100) -> None:
+
+        # * Initialize the parent class NeuralNetwork
+        super(Mlp, self).__init__(num_hidden_layers, num_neurons_hidden,
+                                  input_size, output_size,
+                                  learning_rate, batch_size, epochs, MlpLayer)
+    # * Override the forward method
+
     def forward(self, x: np.ndarray):
         pass
 
--- NeuralLeet/nn/module.py
+++ NeuralLeet/nn/module.py
@@ -3,23 +3,32 @@ import numpy as np
 
 
 class Neuron:
-    """Base class for neuron network components."""
 
-    def __init__(self) -> None:
-        """Initialize a new Neuron instance."""
-        pass
+    def __init__(self, num_inputs: int):
+        self.num_inputs = num_inputs
+        self.weights = np.random.randn(num_inputs)
+        self.bias = 0
 
 
-class Layer(Neuron, ABC):
-    """Abstract base class for layers."""
+class Layer(ABC):
+    '''
+        Layer class for neural network models.
+        Parameters:
+            - num_neurons: int
+                The number of neurons in the layer.
+            - num_inputs: int
+                The number of inputs to the each neuron in the current layer.
+        Returns:
+            None
+    '''
 
-    def __init__(self) -> None:
-        """Initialize a new Layer instance."""
-        super().__init__()
-        pass
+    def __init__(self, num_neurons: int, num_inputs: int) -> None:
+        self.num_neurons = num_neurons
+        self.num_inputs = num_inputs
+        self.neurons = [Neuron(self.num_inputs) for _ in range(num_neurons)]
 
     @abstractmethod
-    def forward(self, x: np.ndarray) -> np.ndarray:
+    def forward(self, x_batch: np.ndarray) -> np.ndarray:
         pass
 
     @abstractmethod
@@ -27,12 +36,14 @@ class Layer(Neuron, ABC):
         pass
 
 
-class NeuralNetwork(Layer, ABC):
+class NeuralNetwork(ABC):
     '''
         Abstract base class for neural network models.
         Parameters:
-            - num_layers: int
+            - num_hidden_layers: int
                 The number of layers in the neural network.
+            - num_neurons_hidden: int
+                The number of neurons in each hidden layer.
             - input_size: int
                 The size of the input layer.
             - output_size: int
@@ -46,12 +57,44 @@ class NeuralNetwork(Layer, ABC):
     '''
 
     def __init__(
-        self, num_layers: int, input_size: int, output_size: int,
-            learning_rate: float = 0.01, batch_size: int = 128) -> None:
+        self, num_hidden_layers: int, num_neurons_hidden: int,
+        input_size: int, output_size: int,
+            learning_rate: float, batch_size: int, epochs: int,
+            layer_type: Layer = Layer
+    ) -> None:
 
-        super().__init__()
-        self.number_of_layers = num_layers
+        self.num_of_hidden_layers = num_hidden_layers
+        self.num_neurons_hidden = num_neurons_hidden
         self.input_size = input_size
         self.output_size = output_size
         self.learning_rate = learning_rate
         self.batch_size = batch_size
+        self.epochs = epochs
+        self.layers = []
+        for i in range(num_hidden_layers):
+            if i == 0:
+                self.layers.append(layer_type(num_neurons_hidden, input_size))
+            else:
+                self.layers.append(
+                    layer_type(num_neurons_hidden, num_neurons_hidden))
+        self.layers.append(layer_type(output_size, num_neurons_hidden))
+
+    def train(self, x: np.ndarray, y: np.ndarray) -> None:
+        '''
+            Train the neural network model.
+            Parameters:
+                - x: np.ndarray
+                    The input data.
+                - y: np.ndarray
+                    The target data.
+            Returns:
+                None
+        '''
+
+        # todo add debugging print statements for ech epoch
+        for epoch in range(self.epochs):
+            for i in range(0, len(x), self.batch_size):
+                x_batch = x[i:i + self.batch_size]
+                y_batch = y[i:i + self.batch_size]
+                for layer in self.layers:
+                    x_batch = layer.forward(x_batch)
