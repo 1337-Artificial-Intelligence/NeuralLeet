08d2cbda28ba37dfa5a825835665273cb183bdc6
khalid benlyazid <seven@khalids-MacBook-Air-1318.local>
2025-03-09 16:16:21 +0000

    feat: update the backward to ude derivative of loss function

--- NeuralLeet/nn/mlp.py
+++ NeuralLeet/nn/mlp.py
@@ -1,5 +1,6 @@
 from NeuralLeet.nn.module import Layer, NeuralNetwork
 import numpy as np
+from ..core.functional import ActivationFunction, LossFunction
 
 
 class MlpLayer(Layer):
@@ -16,15 +17,20 @@ class MlpLayer(Layer):
             The derivative of the activation function
         - learning_rate: float
             The learning rate of the neural network.
+        - loss: callable
+            The loss function to use for training the neural network.
+        - loss_derivative: callable
+            The derivative of the loss function.
         - is_output_layer: bool
             A flag to indicate if the layer is the output
     '''
 
     def __init__(self, num_neurons: int, num_inputs: int,
-                 activation_function: callable,
-                 activation_function_derivative: callable,
+                 activation_function: str,
                  learning_rate: float,
-                 is_output_layer=False):
+                 loss: str = None,
+                 is_output_layer=False
+                 ) -> None:
         '''
             Constructor for the MlpLayer class.
             Parameters:
@@ -39,8 +45,8 @@ class MlpLayer(Layer):
                 None
         '''
         super().__init__(num_neurons, num_inputs, activation_function,
-                         activation_function_derivative,
-                         learning_rate, is_output_layer)
+                         learning_rate, loss,
+                         is_output_layer)
 
     def forward(self, x_batch: np.ndarray) -> np.ndarray:
         """
@@ -53,16 +59,17 @@ class MlpLayer(Layer):
                     The output of the layer
         """
         output = np.dot(self.weights, x_batch) + self.bias
-        activations = self.activation_function(output)
+        activations = self.activation_function.function(output)
         self.last_input = x_batch
         if self.is_output_layer:
-            self.last_output = activations
+            self.last_output = output
+            self.last_activation = activations
         else:
             self.last_output = output
         return activations
 
     def backward(self, y_true: np.ndarray,
-                 weights_of_next_layer: np.ndarray,
+                 weights_of_previous_layer: np.ndarray,
                  last_error_derivative: np.ndarray = None) -> np.ndarray:
         """
         Backward pass with proper handling of activation derivatives
@@ -73,24 +80,43 @@ class MlpLayer(Layer):
             - last_error_derivative: Gradient from
                 the next layer or output error
         """
-        # ? d_z is the gradient of the loss with
-        # ? respect to the output of the layer
+
         if self.is_output_layer:
-            d_z = self.last_output - y_true
+            '''
+                For the output layer, we should compute the derivative
+                of the loss function with respect to the last activation
+                (dL_da). Then, we compute the derivative of the activation
+                function with respect to the linear combination (z), da_dz.
+                Finally, we compute the delta (error term) for the output
+                layer, d_z.
+            '''
+            if self.loss.name == "cross_entropy" and self.activation_function.name == "sigmoid":
+                delta = self.last_activation - y_true
+            else:
+                dL_da = self.loss.derivative(self.last_activation, y_true)
+                da_dz = self.activation_function.derivative(self.last_output)
+                delta = dL_da * da_dz
         else:
-            d_z = np.dot(weights_of_next_layer.T, last_error_derivative) * \
-                self.activation_function_derivative(self.last_output)
+            '''
+                For hidden layers, we should compute the derivative
+                of the activation function with respect to the linear
+                combination (z), da_dz. Finally, we compute the delta
+                (error term) for the hidden layers, d_z.
+            '''
+            delta = np.dot(weights_of_previous_layer.T, last_error_derivative) * \
+                self.activation_function.derivative(self.last_output)
 
-        # ? d_w is the gradient of the loss with
-        # ? respect to the weights of the layer
-        d_w = np.dot(d_z, self.last_input.T) / self.last_input.shape[1]
+        # d_w is the gradient of the loss with
+        d_w = np.dot(delta, self.last_input.T) / self.last_input.shape[1]
 
-        # ? d_b is the gradient of the loss with
-        # ?   respect to the bias of the layer
-        d_b = np.sum(d_z, axis=1, keepdims=True) / self.last_input.shape[1]
+        # d_b is the gradient of the loss with\
+        #  respect to the bias of the layer
+        d_b = np.sum(delta, axis=1, keepdims=True) / self.last_input.shape[1]
+
+        # Update the weights and biases of the layer
         self.weights -= self.learning_rate * d_w
         self.bias -= self.learning_rate * d_b
-        return d_z
+        return delta
 
 
 class Mlp(NeuralNetwork):
@@ -111,25 +137,21 @@ class Mlp(NeuralNetwork):
             The batch size of the neural network.
         - epochs: int
             The number of epochs to train the neural network.
-        - h_activation: callable
+        - h_activation: ActivationFunction
             The activation function for the hidden layers.
-        - o_activation: callable
+        - o_activation: ActivationFunction
             The activation function for the output layer
-        - h_activation_derivative: callable
-            The derivative of the activation function for the hidden layers.
-        - o_activation_derivative: callable
-            The derivative of the activation function for the output layer.
-        - loss: callable
+        - loss: LossFunction
             The loss function to use for training the neural network.
     '''
 
     def __init__(
             self, num_hidden_layers: int,
             num_neurons_hidden: int, input_size: int,
-            h_activation: str, o_activation: str,
+            h_activation: ActivationFunction, o_activation: ActivationFunction,
             output_size: int, learning_rate: float,
             batch_size: int, epochs: int,
-            loss: callable
+            loss: LossFunction
     ) -> None:
 
         # * Initialize the parent class NeuralNetwork
@@ -149,7 +171,7 @@ class Mlp(NeuralNetwork):
                     x_copy = layer.forward(x_copy)
 
                 # * calculate loss
-                loss = self.loss(x_copy, y_copy)
+                loss = self.loss.function(x_copy, y_copy)
 
                 # * calculate gradients
                 last_layer_weights = None
@@ -158,7 +180,7 @@ class Mlp(NeuralNetwork):
                     last_error_derivative = layer.backward(
                         y_copy, last_layer_weights, last_error_derivative)
                     last_layer_weights = layer.weights
-                if (e + 1) % 10 == 0:
+                if (e + 1) % 100 == 0:
                     print(f"Epoch {e+1}/{self.epochs}, Loss: {loss:.4f}")
 
     def predict(self, x: np.ndarray) -> np.ndarray:
